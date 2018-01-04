8b686a7c77cb2a3682db43f82a7f7a51c41913dd
song <song@192.168.0.5>
2018-01-04 16:30:22 +0900

    리팩토리중

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -1,6 +1,7 @@
 package http;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
@@ -10,9 +11,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class HttpRequest {
     private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
+    
+    private RequestLine requestLine;	 //요청라인
+    Map<String, String> headers = new HashMap<String, String>();//요청헤드
+    Map<String, String> params = new HashMap<String, String>();//쿼리스트링 파라미터
 
     public HttpRequest(InputStream is) {
     	try {
@@ -21,37 +27,44 @@ public class HttpRequest {
     		if(line == null) {
     			return;
     		}
+    		//(1),(2)요청라인
+    		requestLine = new RequestLine(line);
     		
-    		//String url = HttpRequestUtils.getUrl(line);
-    		Map<String, String> headers = new HashMap<String, String>();
-    		
-    		while(!"".equals(line)) {
+        	//(3)요청헤더
+        	line = br.readLine();	
+    		while(!line.equals("")) {
     			log.debug("header : {}",line);
+    			String[] headerTokens = line.split(":");	//요청헤더는 필드명:필드값 형태로 이루어져 있음
+   				headers.put(headerTokens[0].trim(), headerTokens[1].trim());
     			line = br.readLine();
-    			String[] headerTokens = line.split(": ");
-    			if(headerTokens.length == 2) {
-    				headers.put(headerTokens[0], headerTokens[1]);
-    			}
     		}
-    		log.debug("Content-Length : {}", headers.get("Content-Length"));
-    	}catch(Exception e) {
-    		e.getStackTrace();
+    		
+    		//(4)요청본문
+    		if(getMethod().equals("POST")) {
+    			String requestBody = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+    			params = HttpRequestUtils.parseQueryString(requestBody);
+    		}else {
+    			params = requestLine.getParams();
+    		}
+    	}catch(IOException e) {
+    		log.error(e.getMessage());
     	}
     }
 
+
     public String getMethod() {
-        return null;
+        return requestLine.getMethod();
     }
 
     public String getPath() {
-        return null;
+        return requestLine.getPath();
     }
 
     public String getHeader(String name) {
-        return null;
+        return headers.get(name);
     }
 
     public String getParameter(String name) {
-        return null;
+        return params.get(name);
     }
 }
--- src/main/java/http/RequestLine.java
+++ src/main/java/http/RequestLine.java
@@ -1,20 +1,41 @@
 package http;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import util.HttpRequestUtils;
+
 class RequestLine {
     private static final Logger log = LoggerFactory.getLogger(RequestLine.class);
 
     private String method;
-
     private String path;
 
-    private Map<String, String> params;
+    private Map<String, String> params = new HashMap<String, String>();
 
     RequestLine(String requestLine) {
+    	log.debug("request line : {}", requestLine);
+    	String[] splited = requestLine.split(" ");	//첫째줄, 요청라인 == 메소드 URL http버전 
+    	//(1)URL
+    	method = splited[0];	
+    	
+    	// (2)path(url) 구하기
+    	if(method.equals("POST"))	{
+    		path = splited[1];
+    		return;
+    	}
+    	int index = splited[1].indexOf("?");
+    	if(index == -1) {
+    		//post 방식일 때
+    		path = splited[1];
+    	} else {
+    		//get 방식일 때
+    		path = splited[1].substring(0, index);
+    		params = HttpRequestUtils.parseQueryString(splited[1].substring(index+1));
+    	}
     }
 
     String getMethod() {
